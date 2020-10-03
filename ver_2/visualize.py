fed384ca289a914d86df07ab44474b2da2397b16
Nong Minh Hieu <hieubkvn123@gmail.com>
2020-10-03 07:27:24 -0400

    ahihi

--- ver_2/visualize.py
+++ ver_2/visualize.py
@@ -31,22 +31,22 @@ plt.style.use('dark_background')
 plt.rcParams['savefig.facecolor'] = 'white'
 
 ax1[0][0].plot(data_frame['epoch'], data_frame['loss'], color='blue', label='Total Loss', linewidth=2)
-ax1[0][0].plot(data_frame['epoch'], data_frame['val_loss'], color='red', label='Total Validation Loss', linewidth=2)
+ax1[0][0].plot(data_frame['epoch'], data_frame['val_loss'].iloc[::-1], color='red', label='Total Validation Loss', linewidth=2)
 ax1[0][0].set_title('Total Losses')
 ax1[0][0].legend()
 
 ax1[0][1].plot(data_frame['epoch'], data_frame['surface_final_output_loss'], color='blue', label='Surface Loss', linewidth=2)
-ax1[0][1].plot(data_frame['epoch'], data_frame['val_surface_final_output_loss'], color='red', label='Surface Validation Loss', linewidth=2)
+ax1[0][1].plot(data_frame['epoch'], data_frame['val_surface_final_output_loss'].iloc[::-1], color='red', label='Surface Validation Loss', linewidth=2)
 ax1[0][1].set_title('Total losses (Surface prediction)')
 ax1[0][1].legend()
 
 ax1[1][0].plot(data_frame['epoch'], data_frame['edge_final_output_loss'], color='blue', label='Edge Loss', linewidth=2)
-ax1[1][0].plot(data_frame['epoch'], data_frame['val_edge_final_output_loss'], color='red', label='Edge Validation Loss', linewidth=2)
+ax1[1][0].plot(data_frame['epoch'], data_frame['val_edge_final_output_loss'].iloc[::-1], color='red', label='Edge Validation Loss', linewidth=2)
 ax1[1][0].set_title('Total losses (Edge prediction)')
 ax1[1][0].legend()
 
 ax1[1][1].plot(data_frame['epoch'], data_frame['line_final_output_loss'], color='blue', label='Line Loss', linewidth=2)
-ax1[1][1].plot(data_frame['epoch'], data_frame['val_line_final_output_loss'], color='red', label='Line Validation Loss', linewidth=2)
+ax1[1][1].plot(data_frame['epoch'], data_frame['val_line_final_output_loss'].iloc[::-1], color='red', label='Line Validation Loss', linewidth=2)
 ax1[1][1].set_title('Total losses (Center-line prediction)')
 ax1[1][1].legend()
 
