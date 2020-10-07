a6e85bb7b687f34a791b62f6f6ad33b1f28d67bd
Nong Minh Hieu <hieubkvn123@gmail.com>
2020-10-07 09:17:34 -0400

    ahihi

--- ver_2/to_raster.py
+++ ver_2/to_raster.py
@@ -2,19 +2,43 @@ import rasterio
 import rasterio.features
 import rasterio.warp
 
-def to_geojson(image_file):  
-    profile = {'driver': 'GTiff', 'height': 100, 'width': 100, 'count': 1, 'dtype': rasterio.uint8}
-    with rasterio.open(image_file, **profile) as dataset:
+from osgeo import gdal, osr
+
+src_filename = '../data/2/segmentation.png'
+dst_filename = 'destination.tif'
+
+src_ds = gdal.Open(src_filename)
+format = "GTiff"
+driver = gdal.GetDriverByName(format)
+
+# Open destination dataset
+dst_ds = driver.CreateCopy(dst_filename, src_ds, 0)
+
+# Specify raster location through geotransform array
+# (uperleftx, scalex, skewx, uperlefty, skewy, scaley)
+# Scale = size of one pixel in units of raster projection
+# this example below assumes 100x100
+gt = [-75.777511597, 7936, 0, 45.340576172, 0, 4864]
+
+dst_ds.SetGeoTransform(gt)
+
+epsg = 4326
+srs = osr.SpatialReference()
+srs.ImportFromEPSG(epsg)
+dest_wkt = srs.ExportToWkt()
+
+dst_ds.SetProjection(dest_wkt)
+
+dst_ds = None
+src_ds = None
+
+def to_geojson(file_name):
+    with rasterio.open(file_name) as dataset:
         mask = dataset.dataset_mask()
 
-        ### Extract feature shapes and values from the array ###
         for geom, val in rasterio.features.shapes(mask, transform=dataset.transform):
-            print(dataset.crs)
-            geom = rasterio.warp.transform_geom(
-                dataset.crs,
-                'EPSG:3857',
-                geom,
-                precision=6
-            )
+            geom = rasterio.warp.transform_geom(dataset.crs, 'EPSG:4326', geom, precision=6)
 
             print(geom)
+
+to_geojson(dst_filename)
