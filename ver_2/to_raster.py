778ec73734256e061b89533125bd4e5d45996ad7
Nong Minh Hieu <hieubkvn123@gmail.com>
2020-10-07 11:34:36 -0400

    ahihi

--- ver_2/to_raster.py
+++ ver_2/to_raster.py
@@ -11,34 +11,8 @@ from osgeo import gdal, osr
 src_filename = '../data/2/segmentation.png'
 dst_filename = 'destination.tif'
 
-src_ds = gdal.Open(src_filename)
-format = "GTiff"
-driver = gdal.GetDriverByName(format)
-
-# Open destination dataset
-dst_ds = driver.CreateCopy(dst_filename, src_ds, 0)
-
-# Specify raster location through geotransform array
-# (uperleftx, scalex, skewx, uperlefty, skewy, scaley)
-# Scale = size of one pixel in units of raster projection
-# this example below assumes 100x100
-gt = [-75.777511597, 7936, 0, 45.340576172, 0, 4864]
-
-dst_ds.SetGeoTransform(gt)
-
-epsg = 4326
-srs = osr.SpatialReference()
-srs.ImportFromEPSG(epsg)
-dest_wkt = srs.ExportToWkt()
-
-dst_ds.SetProjection(dest_wkt)
-
-dst_ds = None
-src_ds = None
-
 feature_collection = {}
 feature_collection['type'] = 'FeatureCollection'
-feature_collection['properties'] = {'props':'val'}
 feature_collection['features'] = []
 
 def to_geojson(file_name):
@@ -46,7 +20,7 @@ def to_geojson(file_name):
         blue = src.read(3)
 
     mask = blue != 255
-    shapes = rasterio.features.shapes(blue, mask=mask)
+    shapes = rasterio.features.shapes(blue, mask=mask, transform=src.transform)
 
     object_ = {}
     object_['type'] = 'Feature'
@@ -55,15 +29,17 @@ def to_geojson(file_name):
     for shape in shapes:
         print('[*] Sparsing shape #{:04d}'.format(counter + 1))
         object_['type'] = 'Feature'
-        object_['properties'] = {'prop{:01d}'.format(counter) : 'prop{:01d}'.format(counter)}
         object_['geometry'] = shape
-
+        object_['properties'] = {'prop{:01d}'.format(counter) : 'prop{:01d}'.format(counter)}
+        
         feature_collection['features'].append(object_)
         counter += 1
 
+    feature_collection['properties'] = {'props':'val'}
+    
     filename = 'geojson/obj_%.2f.geojson' % time.time()
     print('[*] Writing to file : %s' % filename)
     json.dump(feature_collection, open(filename, 'w'), indent=2)
 
 
-to_geojson(dst_filename)
+to_geojson(src_filename)
