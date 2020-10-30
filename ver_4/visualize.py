6f84ef59154cfe88129de5bc0e4fffffb4ad19f6
Nong Minh Hieu <hieubkvn123@gmail.com>
2020-10-30 19:36:05 +0700

    ahihi

--- ver_4/visualize.py
+++ ver_4/visualize.py
@@ -26,7 +26,6 @@ data_frame = data_frame[columns]
 data_frame['epoch'] = list(range(len(data_frame['epoch'])))
 
 fig1, ax1 = plt.subplots(2,2, figsize=(15,15))
-fig2, ax2 = plt.subplots(3,1, figsize=(10, 20))
 plt.style.use('dark_background')
 plt.rcParams['savefig.facecolor'] = 'white'
 
@@ -50,31 +49,11 @@ ax1[1][1].plot(data_frame['epoch'], data_frame['val_line_final_output_loss'].ilo
 ax1[1][1].set_title('Total losses (Center-line prediction)')
 ax1[1][1].legend()
 
-ax2[0].plot(data_frame['epoch'], data_frame['surface_final_output_mean_io_u'], color='blue', label='Surface MeanIOU', linewidth=2)
-ax2[0].plot(data_frame['epoch'], data_frame['val_surface_final_output_mean_io_u'], color='red', label='Surface Validation MeanIOU', linewidth=2)
-ax2[0].set_title('MeanIOU (Surface prediction)')
-ax2[0].legend()
-
-ax2[1].plot(data_frame['epoch'], data_frame['edge_final_output_mean_io_u'], color='blue', label='Edge MeanIOU', linewidth=2)
-ax2[1].plot(data_frame['epoch'], data_frame['val_edge_final_output_mean_io_u'], color='red', label='Edge Validation MeanIOU', linewidth=2)
-ax2[1].set_title('MeanIOU (Edge prediction)')
-ax2[1].legend()
-
-ax2[2].plot(data_frame['epoch'], data_frame['line_final_output_mean_io_u'], color='blue', label='Center-line MeanIOU', linewidth=2)
-ax2[2].plot(data_frame['epoch'], data_frame['val_line_final_output_mean_io_u'], color='red', label='Center-line Validation MeanIOU', linewidth=2)
-ax2[2].set_title('MeanIOU (Center-line prediction)')
-ax2[2].legend()
-
 for ax_ in ax1:
     for ax in ax_:
         ax.set_facecolor('black')
         ax.grid(color='gray')
 
-for ax in ax2:
-    ax.set_facecolor('black')
-    ax.grid(color='gray')
-
 fig1.savefig('training_results/losses.png')
-fig2.savefig('training_results/mean_io_u.png')
 
 plt.show()
